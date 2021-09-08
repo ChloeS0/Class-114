99fe4abc9e536d282439070791f8bf1faddccaa7
ChloeS0 <74438079+ChloeS0@users.noreply.github.com>
2021-09-07 19:43:23 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+function setup() {
+  canvas=createCanvas(300, 300);
+  canvas.center();
+}
+
+function draw() {
+  background(220);
+}
+
+function preload()
+{
+  
+  
+}
+
+function take_snapshot()
+{
+  save("ImageFilter.png")
+}
